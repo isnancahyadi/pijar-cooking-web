bf838bce97e3b50d298a843cba397257b7495507
isnancahyadi <isnan.arifc@gmail.com>
2023-08-22 15:19:24 +0700

    [ADD] sentry and some information

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as Sentry from "@sentry/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+Sentry.init({
+  dsn: "https://8609856256f798009346cd684e91b7a6@o4505384079851520.ingest.sentry.io/4505747454492672",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: [
+        "localhost",
+        "https://pijar-cooking-web.vercel.app/",
+      ],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
